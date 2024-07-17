39350b389bf5d79ff004f8446fd20f9c80021080
Veeresh-Lakkundimath-au49 <107140637+Veeresh-Lakkundimath-au49@users.noreply.github.com>
2024-07-17 13:53:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,28 @@
 
-const express = require('express');
-const cron = require('node-cron');
+// const express = require('express');
+// const cron = require('node-cron');
 
+// const app = express();
+// cron.schedule('*/2 * * * * *', async () => {
+//   console.log('Hello, World!');
+// //   await firebase_test();
+// });
+
+// console.log("first console of the index.js on 31 October 2023")
+
+
+// app.get('/',(req,res)=>{
+//     console.log('Hello, World! from the home route');
+//     res.send("Server started successfully");
+// })
+
+const express = require("express");
 const app = express();
-cron.schedule('*/2 * * * * *', async () => {
-  console.log('Hello, World!');
-//   await firebase_test();
-});
 
-console.log("first console of the index.js on 31 October 2023")
+app.get("/", (req, res) => res.send("Express on Vercel"));
 
+app.listen(3000, () => console.log("Server ready on port 3000."));
 
-app.get('/',(req,res)=>{
-    console.log('Hello, World! from the home route');
-    res.send("Server started successfully");
-})
+module.exports = app;
 
-module.exports = app; // Export the app for Vercel deployment
+// module.exports = app; // Export the app for Vercel deployment
