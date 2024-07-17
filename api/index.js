428b8950767cee817b005fd004302c947d8a4c4b
Veeresh-Lakkundimath-au49 <107140637+Veeresh-Lakkundimath-au49@users.noreply.github.com>
2024-07-17 16:55:05 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -19,6 +19,7 @@
 const express = require("express");
 const app = express();
 
+console.log("Hello world from new project");
 app.get("/", (req, res) => res.send("Express on Vercel"));
 
 app.listen(3000, () => console.log("Server ready on port 3000."));
