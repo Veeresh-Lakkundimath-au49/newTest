19be0d8310d920de4d6b2d0d028dcad8c2f44fe0
Veeresh-Lakkundimath-au49 <107140637+Veeresh-Lakkundimath-au49@users.noreply.github.com>
2024-07-17 11:28:12 +0530

    added cron fir testing

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log("first console of the index.js on 31 October 2023")
+// console.log("first console of the index.js on 31 October 2023")
+
+const cron = require('node-cron');
+
+cron.schedule('*/2 * * * * *', async () => {
+  console.log('Hello, World!');
+//   await firebase_test();
+});
