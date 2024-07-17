f8c23398e466a9d9901c086f736c1817cd003bab
Veeresh-Lakkundimath-au49 <107140637+Veeresh-Lakkundimath-au49@users.noreply.github.com>
2024-07-17 12:01:12 +0530

    express

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 // console.log("first console of the index.js on 31 October 2023")
 
+const express = require('express');
 const cron = require('node-cron');
 
+const app = express();
 cron.schedule('*/2 * * * * *', async () => {
   console.log('Hello, World!');
 //   await firebase_test();
 });
+
+
+app.get('/',(req,res)=>{
+    console.log('Hello, World! from the home route');
+    res.send("Server started successfully");
+})
