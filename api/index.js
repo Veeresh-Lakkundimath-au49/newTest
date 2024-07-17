78d073b14bce481137ea73449217518f78937153
Veeresh-Lakkundimath-au49 <107140637+Veeresh-Lakkundimath-au49@users.noreply.github.com>
2024-07-17 17:42:14 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -20,23 +20,23 @@ const express = require("express");
 const app = express();
 cron.schedule('*/2 * * * * *', async () => {
     console.log('Hello, World cron job!');
-    // try {
-    //     let info = await fetch("https://new-test-express.vercel.app/");
-    //     const data = await info.text();
-    //     console.log("data: ",data);
-    // } catch (error) {
-    //     console.log("error in cron: ",error);
-    // }
+    try {
+        let info = await fetch("https://new-test-express.vercel.app/");
+        const data = await info.text();
+        console.log("data: ",data);
+    } catch (error) {
+        console.log("error in cron: ",error);
+    }
   //   await firebase_test();
   });
 
 
 
-// console.log("Hello world from new project");
-// app.get("/", (req, res) => res.send("Express on Vercel"));
+console.log("Hello world from new project");
+app.get("/", (req, res) => res.send("Express on Vercel"));
 
-// app.listen(3000, () => console.log("Server ready on port 3000."));
+app.listen(3000, () => console.log("Server ready on port 3000."));
 
-// module.exports = app;
+module.exports = app;
 
 module.exports = app; // Export the app for Vercel deployment
